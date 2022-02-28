62af938e6549871a4543b2b8ef06512997bcacf7
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-02-28 18:25:38 +0300

    Add toggle menu when menu item clicked

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,15 @@
 const toggleMenuButton = document.getElementById('toggle-menu');
 const menu = document.getElementById('menu');
+const menuItems = document.querySelectorAll('.nav-menu-item');
 
 toggleMenuButton.addEventListener('click', () => {
   toggleMenuButton.classList.toggle('active');
   menu.classList.toggle('active');
 });
 
-
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    toggleMenuButton.classList.remove('active');
+    menu.classList.remove('active');
+  });
+});
