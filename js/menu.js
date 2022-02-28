c4e0894aca08c27e13ad93c1f37004f0791f2cbb
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-02-28 16:49:17 +0300

    Fix linter error

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
+const toggleMenuButton = document.getElementById('toggle-menu');
+const menu = document.getElementById('menu');
 
-const toggleMenuButton = document.getElementById("toggle-menu");
-const menu = document.getElementById("menu");
-
-toggleMenuButton.addEventListener("click", () => {
-  toggleMenuButton.classList.toggle("active");
-  menu.classList.toggle("active");
+toggleMenuButton.addEventListener('click', () => {
+  toggleMenuButton.classList.toggle('active');
+  menu.classList.toggle('active');
 });
