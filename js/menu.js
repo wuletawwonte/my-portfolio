55807a88989a24f29da09be0b71eb4000ca07050
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-01 18:10:09 +0300

    Fix scroll issue

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,12 @@
 const toggleMenuButton = document.getElementById('toggle-menu');
 const menu = document.getElementById('menu');
 const menuItems = document.querySelectorAll('.nav-menu-item');
+const mainBody = document.querySelector('body');
 
 toggleMenuButton.addEventListener('click', () => {
   toggleMenuButton.classList.toggle('active');
   menu.classList.toggle('active');
+  mainBody.classList.toggle('hide-overflow');
 });
 
 menuItems.forEach((item) => {
