83fd2d7766e5f0abd4657de8271d960d3cda6aeb
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-02 13:39:51 +0300

    Fix scroll problem using absolute position

--- js/projectlist.js
+++ js/projectlist.js
@@ -1,63 +1,63 @@
 const showPopup = (pid) => {
-  const popupProject = projects.find(item => {
-    if(pid == item.id) {
+  const popupProject = projects.find((item) => {
+    if (pid == item.id) {
       return item;
     }
   });
   popupContainer.innerHTML = popupContent(popupProject);
   wrapper.classList.add('hidden');
-  window.scrollTo(0,0);
+  window.scrollTo(0, 0);
 };
 
 const projects = [
   {
     id: 1,
-    name: "Ethiopia Project",
+    name: 'Ethiopia Project',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    featured_image: "Rectangle_21.jpg",
-    technologies: ["HTML/CSS", "Ruby on Rails", "Javascript"],
-    live_link: "#",
-    source_link: "#",
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    live_link: '#',
+    source_link: '#',
   },
   {
     id: 2,
-    name: "Project Two",
+    name: 'Project Two',
     description:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    featured_image: "Rectangle_21.jpg",
-    technologies: ["HTML/CSS", "Ruby on Rails", "Javascript"],
-    live_link: "#",
-    source_link: "#",
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    live_link: '#',
+    source_link: '#',
   },
   {
     id: 3,
-    name: "Project Three",
+    name: 'Project Three',
     description:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    featured_image: "Rectangle_21.jpg",
-    technologies: ["HTML/CSS", "Ruby on Rails", "Javascript"],
-    live_link: "#",
-    source_link: "#",
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    live_link: '#',
+    source_link: '#',
   },
   {
     id: 4,
-    name: "Project Four",
+    name: 'Project Four',
     description:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.",
-    featured_image: "Rectangle_21.jpg",
-    technologies: ["HTML/CSS", "Ruby on Rails", "Javascript"],
-    live_link: "#",
-    source_link: "#",
+      'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.., comes from a line in section 1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
+    featured_image: 'Rectangle_21.jpg',
+    technologies: ['HTML/CSS', 'Ruby on Rails', 'Javascript'],
+    live_link: '#',
+    source_link: '#',
   },
 ];
 
-const projectContainer = document.getElementById("projectsContainer");
-const popupContainer = document.getElementById("project-detail");
+const projectContainer = document.getElementById('projectsContainer');
+const popupContainer = document.getElementById('project-detail');
 const wrapper = document.querySelector('.wrapper');
 
 function hidePopup() {
-  popupContainer.innerHTML = "";
+  popupContainer.innerHTML = '';
   wrapper.classList.remove('hidden');
 }
 
@@ -79,7 +79,7 @@ const renderedProjects = projects
             </div>
           `;
   })
-  .join("");
+  .join('');
 
 projectContainer.innerHTML = renderedProjects;
 
@@ -89,13 +89,15 @@ const popupContent = (project) => {
         <button class="modal-close-btn" onclick="hidePopup()">&times;</button>
         <h2>${project.name}</h2>
         <ul class="modal-technologies">
-          ${project.technologies.map((technology) => {
-            return `
+          ${project.technologies
+            .map((technology) => {
+              return `
                 <li class="single-technology">
                   <a href="#">${technology}</a>
                 </li>
             `;
-          }).join('')}
+            })
+            .join('')}
         </ul>
         <div class="caroussel">
           <img src="img/projects/${project.featured_image}" />
@@ -104,10 +106,10 @@ const popupContent = (project) => {
           <p>${project.description}</p>
           <div class="project-buttons">
             <a href="${project.live_link}"
-              ><span>See live</span><img src="img/Union.svg"
+              ><span>See live</span><img src="img/ic_link.svg"
             /></a>
             <a href="${project.source_link}"
-              ><span>See source</span><img src="img/social/ic_github.svg"
+              ><span>See source</span><img src="img/ic_github_white.svg"
             /></a>
           </div>
         </div>
