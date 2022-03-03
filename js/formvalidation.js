3fa28ab08e3056583647157f53164fb7b220c890
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-03-03 12:25:59 +0300

    Make required changes

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,12 +1,15 @@
 const contactForm = document.querySelector('.contact-form');
 const emailContainer = document.querySelector('.email-container');
 const emailInput = document.getElementById('email');
+const errorMessageContainer = document.querySelector('.error-message');
+const emailErrorMessage = "Please enter email address in small letters."; 
 
 const validateEmail = () => {
   const lowercaseEmail = emailInput.value.toLowerCase();
   if (lowercaseEmail !== emailInput.value) {
     emailContainer.classList.add('email-error');
     emailInput.focus();
+    errorMessageContainer.textContent = emailErrorMessage;
     return false;
   }
   emailContainer.classList.remove('email-error');
